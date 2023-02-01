c72375874cbae339b13e913225128abdc9946116
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-02-01 10:37:10 +0100

    :wrench: try to handle error

---
+++ src/ts/dataArrayMock.ts
@@ -0,0 +1,7 @@
+import { IMovie } from "./models/IMovie";
+
+export let testData:IMovie[] = [
+    {Title: "Bb", imdbID: "1234", Type:"Bction", Poster:"None", Year:"2022" },
+    {Title: "Aa", imdbID: "4321", Type:"Action", Poster:"Done", Year:"2023" },
+    {Title: "Cc", imdbID: "5678", Type:"Cction", Poster:"Bone", Year:"2024" }
+];
